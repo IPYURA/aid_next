c5c1c0e3372d61f4f164820ed865654f501878ec
조동건 <o207@aidsoft.kr>
2025-07-21 17:59:32 +0900

    prettier 추가

--- app/stocks/StockTable.js
+++ app/stocks/StockTable.js
@@ -5,13 +5,13 @@ import { Column } from 'primereact/column';
 function StockTable({ data }) {
     return (
         <DataTable value={data} paginator rows={20}>
-            <Column field='seq' header='Seq' />
-            <Column field='symbol' header='Symbol' />
-            <Column field='date' header='Date' />
-            <Column field='close' header='Close' />
-            <Column field='high' header='High' />
-            <Column field='low' header='Low' />
-            <Column field='open' header='Open' />
+            <Column field="seq" header="Seq" />
+            <Column field="symbol" header="Symbol" />
+            <Column field="date" header="Date" />
+            <Column field="close" header="Close" />
+            <Column field="high" header="High" />
+            <Column field="low" header="Low" />
+            <Column field="open" header="Open" />
         </DataTable>
     );
 }
--- app/stocks/page.js
+++ app/stocks/page.js
@@ -8,6 +8,10 @@ async function Page() {
         console.error('Supabase fetch error:', error.message);
         return <p>데이터를 불러오지 못했습니다.</p>;
     }
-    return <StockTable data={data} />; // 데이터가 props로 즉시 전달됨
+    return (
+        <div>
+            <StockTable data={data} />; // 데이터가 props로 즉시 전달됨
+        </div>
+    );
 }
 export default Page;
